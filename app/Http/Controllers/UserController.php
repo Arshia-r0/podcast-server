3290760bc68d814337a44554d6116199cf9a02fd
Arshia-r0 <arshia009k@proton.me>
2024-11-08 15:08:50 +0330

    .

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Request;
+
+class UserController extends Controller
+{
+
+    function login(Request $request): JsonResponse
+    {
+        $credentials = $request->validate([
+            "user_name" => "required",
+            "password" => "required"
+        ]);
+
+        $user = User::where("user_name", $credentials["user_name"])->first();
+
+        if (!$user || !Hash::check($credentials["password"], $user->password)) {
+            return response()->json([
+                "message" => "The provided credentials are incorrect."
+            ], 401);
+        }
+
+        return response()->json([
+            "message" => "Logged in successfully.",
+            "access_token" => $user->createToken($user->name . "-AuthToken")->plainTextToken
+        ]);
+    }
+
+    function logout(): JsonResponse
+    {
+        auth()->user()->tokens()->delete();
+        return response()->json(["message" => "Logged out successfully."]);
+    }
+
+    function register(Request $request): JsonResponse
+    {
+        $registerUserData = $request->validate([
+            'user_name' => 'required|string|unique:users',
+            'password' => 'required|min:8'
+        ]);
+        $user = User::create([
+            'user_name' => $registerUserData['user_name'],
+            'password' => Hash::make($registerUserData['password']),
+        ]);
+        return response()->json([
+            'message' => 'Registered successfully.',
+            "access_token" => $user->createToken($user->name . "-AuthToken")->plainTextToken
+        ]);
+    }
+
+}
